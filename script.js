985125953b090534d40166fcfa5be85d93e517ea
VladimirZivanovic <vladimirzivanovic1497@gmail.com>
2025-07-22 01:51:15 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 
-function showService(text) {
-  document.getElementById('popup-text').innerText = text;
-  document.getElementById('popup').classList.remove('hidden');
-}
-function hidePopup() {
-  document.getElementById('popup').classList.add('hidden');
-}
+document.addEventListener('DOMContentLoaded', function () {
+    const modal = document.getElementById('modal');
+    const closeBtn = document.getElementById('closeBtn');
+
+    closeBtn.addEventListener('click', function () {
+        modal.style.display = 'none';
+    });
+
+    window.addEventListener('click', function (event) {
+        if (event.target === modal) {
+            modal.style.display = 'none';
+        }
+    });
+});
